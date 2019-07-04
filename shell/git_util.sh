fada212440dd7b45c9d77a2e266c1ad022bbbd34
Elvis <1224522500@qq.com>
2019-07-04 11:46:50 +0800

    commitMsg

--- shell/git_util.sh
+++ shell/git_util.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 git status
 git add ./
-git commit -m "Add new script"
+echo "Please input commit message: "
+read commitMsg
+git commit -m commitMsg
 read -n 1 -p "Press any key to start to push..."
 git push
-read -n 1 -p "Press any key to start to push..."
+read -n 1 -p "Press any key to end..."
