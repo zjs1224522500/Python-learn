09b3b37dd03fc0822640d81c28ff1d6f40636254
Elvis <1224522500@qq.com>
2019-07-09 10:05:15 +0800

    Complete 404 page

--- flask_demo/app.py
+++ flask_demo/app.py
@@ -83,7 +83,7 @@ def error():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('static/404/index.html'), 404
+    return render_template('/404/404.html'), 404
 
 
 if __name__ == '__main__':
