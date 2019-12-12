566e21660245f6e863dfbba55f8480147eb4be58
Elvis <1224522500@qq.com>
2019-12-12 21:29:39 +0800

    Complete sync util function

--- shell/blog_sync_to_coding_page.sh
+++ shell/blog_sync_to_coding_page.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 path="D:\zjs1224522500.github.io"
+remote_branch_name="coding"
+remote_branch_url="https://git.coding.net/qcloud_3562257030/Blog.git"
 echo ${path}
 cd ${path}
 dirs
 git status
 git pull --rebase
-git add ./
-git commit -m "SYNC Github Page"
-git push coding
+check_remote_branch=`git remote show ${remote_branch_name}`
+echo -e "command show remote: ${check_remote_branch}"
+
+if [[ ! -n "${check_remote_branch}" ]]
+then
+    echo -e "Not contained.\n"
+    echo "Add new branch with name ${remote_branch_name} and url ${remote_branch_url}"
+    git remote add ${remote_branch_name} ${remote_branch_url}
+else
+    echo "Contained."
+fi
+git push ${remote_branch_name}
 read -n 1 -p "Press any key to continue..."
