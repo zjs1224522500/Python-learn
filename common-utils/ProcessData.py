e3b1b1ffee5b4ae36ad3f1b5d45bb9d0670b03c9
Elvis Zhang <1224522500@qq.com>
2021-08-16 22:59:31 +0800

    Add log extract util

---
+++ common-utils/ProcessData.py
@@ -0,0 +1,78 @@
+import os
+
+path = r"C:\Users\Elvis Zhang\Desktop\round2"
+
+
+class TestData:
+    total_throughput = 0.0
+    read_throughput = 0.0
+    write_throughput = 0.0
+    block_cache_promote_ratio = 0.0
+    block_hit_ratio = 0.0
+    false_positive = 0.0
+    block_cache_reuse_ratio = 0.0
+    block_cache_hit_ratio = 0.0
+
+    # def __init__(self, total, read, write, promote, hit, fp, reuse, cache_hit):
+    #     self.total_throughput = total
+    #     self.read_throughput = read
+    #     self.write_throughput = write
+    #     self.block_cache_hit_ratio = hit
+    #     self.block_cache_promote_ratio = promote
+    #     self.false_positive = fp
+    #     self.block_cache_reuse_ratio = reuse
+    #     self.block_hit_ratio = cache_hit
+
+
+def extract_attr(line, start_str, end_str, is_expresion=False):
+    start = 0
+    end = 0
+    attr = 0.0
+    if line.__contains__(start_str):
+        start = line.index(start_str)
+        if len(end_str) > 0:
+            end = line.index(end_str)
+            attr = float(line[start + len(start_str):end])
+        else:
+            if is_expresion:
+                attr = float(eval(line[start + len(start_str):]))
+            else:
+                attr = float(line[start + len(start_str):])
+    return attr
+
+
+def build_test_data(file_name):
+    file_path = path + "\\" + file_name
+    # print(file_path)
+    file_obj = open(file_path, 'r', encoding='utf-8', errors='ignore')
+    test_data = TestData()
+    lines = file_obj.readlines()
+    for line in lines:
+        line = line.strip().replace('\n', '')
+        if line.__contains__("Write ops: "):
+            test_data.write_throughput = extract_attr(line, "Write ops: ", "ops/s") / 1000
+        if line.__contains__("Read ops: "):
+            test_data.read_throughput = extract_attr(line, "Read ops: ", "ops/s") / 1000
+        if line.__contains__("Transaction throughput (KTPS) "):
+            test_data.total_throughput = extract_attr(line, "Transaction throughput (KTPS) ", "")
+        if line.__contains__("Promote to cache ratio "):
+            test_data.block_cache_promote_ratio = extract_attr(line, "Promote to cache ratio ", "", is_expresion=True)
+        if line.__contains__("Point Query Block Cache Hit ratio "):
+            test_data.block_hit_ratio = extract_attr(line, "Point Query Block Cache Hit ratio ", "", is_expresion=True)
+        if line.__contains__("False Poistive Ratio "):
+            test_data.false_positive = extract_attr(line, "False Poistive Ratio ", "", is_expresion=True)
+
+    test_data.block_cache_reuse_ratio = 1 - test_data.block_cache_promote_ratio
+    test_data.block_cache_hit_ratio = test_data.block_cache_reuse_ratio * (1 - test_data.false_positive)
+    return test_data
+
+
+if __name__ == '__main__':
+    test_dict = {}
+    for file in os.listdir(path):
+        test_dict[file] = build_test_data(file)
+    for kv in test_dict.items():
+        print(kv[0], kv[1].__dict__)
+
+
+
