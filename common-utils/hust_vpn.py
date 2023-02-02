e503e3abe53ee05519d2b34aeecc6fbd9053dd0d
Elvis Zhang <1224522500@qq.com>
2023-02-02 12:38:44 +0800

    Add recover function

--- common-utils/hust_vpn.py
+++ common-utils/hust_vpn.py
@@ -4,6 +4,8 @@ import re
 import sys
 import ctypes
 
+deleted_routes = []
+
 
 def is_admin():
     try:
@@ -12,15 +14,19 @@ def is_admin():
         return False
 
 
-def change_route(routes):
+def change_route(routes, bk):
     # 想要代理的 IP
     # routes = ['222.20.xx.xx', '222.20.xx.xx', ]
     print(routes)
+    print(bk)
 
-    txt = os.popen('route print 222.20.*')
-    txt = txt.read()
+    # 输出 222.20 相关的路由信息
+    pipe = os.popen('route print 222.20.*')
+    txt = pipe.read()
+    # 按照格式匹配
     result = re.search(r' 222.20.* (222.20.*) (222.20.*) ', txt)
     if result:
+        # 解析 mask
         mask = result.group(0).split()[1]
         # 读取网关和接口
         gate = result.group(1).strip()
@@ -30,15 +36,16 @@ def change_route(routes):
         print(f'mask:{mask} gateway:{gate} interface:{inter}')
 
         # 读取对应的路由
-        txt = os.popen('route print')
-        txt = txt.readlines()
+        pipe = os.popen('route print')
+        txt = pipe.readlines()
+        print(len(txt))
         for line in txt:
             # 按照空格分割得到结果
             # 正则表达式 [ ]+
             result = re.split('[ ]+', line.strip())
             # print(result)
 
-            # 提取出的数据长度为 5 的行数据，则为路由
+            # 提取出的数据长度为 5 的行数据，则为路由 (target mask gate interface metric)
             # 只处理 对应路由的网关和接口为我们一开始提取的网关和接口 的路由数据
             # 即找到那些使用了 VPN 的路由
             if len(result) == 5 and result[2].strip() == gate and result[3].strip() == inter:
@@ -47,6 +54,10 @@ def change_route(routes):
                 # 删除这些使用了 VPN 的路由
                 os.system(f'route delete {para1} {gate}')
 
+                # backup
+                cmd = 'route add {} mask 255.255.255.255 {}'.format(para1, gate)
+                deleted_routes.append(cmd)
+
         for route in routes:
             print("routes")
             os.system(f'route add {route} mask 255.255.255.255 {gate}')
@@ -54,25 +65,60 @@ def change_route(routes):
         # print('如果重复运行该程序，因主要网关已被清理将无法找到VPN网关，请尝试优化程序或重启VPN。')
         print("err")
 
+    # backup
+    # input('input:')
+    print("*******Backup******")
+    print(len(deleted_routes))
+    # input('input:')
+
+    if len(deleted_routes) > 0:
+        try:
+            bk_fp = open(bk, "w+")
+            for line in deleted_routes:
+                bk_fp.write(line + "\n")
+
+            bk_fp.close()
+        except Exception as e:
+            print("open exception: %s: %s" % (e.errno, e.strerror))
+
+    input('input:')
+
+
+def recover(path):
+    fp = open(path)
+    for line in fp.readlines():
+        if line.startswith("route"):
+            os.system(line)
+
     input('input:')
 
 
 if __name__ == '__main__':
-    routes = sys.argv[1:]
+    # param: bk_file_path, and routes ip
+    action = sys.argv[1]
+    bk_path = sys.argv[2]
+    routes = sys.argv[3:]
+    print(action)
+    print(bk_path)
     print(routes)
+    # input('input:')
 
     if is_admin():
         print("is admin")
         print("exec ")
-        change_route(routes)
+        if action == "change":
+            change_route(routes, bk_path)
+        elif action == "recover":
+            recover(bk_path)
     else:
         print("not admin")
-        param = __file__ + " " + " ".join(routes)
+        param = __file__ + " " + action + " " + bk_path + " " + " ".join(routes)
         print(param)
         if sys.version_info[0] == 3:
             ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, param, None, 1)
         else:  # in python2.x
             ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(sys.executable), unicode(param), None, 1)
+
 
 
 
